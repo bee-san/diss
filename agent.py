1ef4f7df363fbd327d2992b8632984590d9f4abd
bee <brandonskerritt51@gmail.com>
2020-04-24 22:55:54 +0100

    final

--- agent.py
+++ agent.py
@@ -4,14 +4,30 @@ import random
 
 class agent:
     def __init__(self, maze):
-        this.maze = mz.maze()
+        self.maze = mz.maze()
 
         # we start at (0,0) and we finish at the very bottom right of the maze
         self.start = (0, 0)
         self.goal = self.maze.getSizeOfMaze()
         
         # the reward table is the size of the maze filled with 0's as data type float 32bit
-        self.rewardsTable = np.zeros(self.maze.getSizeOfMaze(), dtype=np.float32)
+        #self.rewardsTable = np.zeros(self.maze.getSizeOfMaze(), dtype=np.float32)
+        # the rewards table is a dictionary of {state, action, reward}
+        # Rewards table should be {"State": {"action": "hello"}}
+        # but if we have to do binarysearch after every action, it'll be slower
+        # it'll be faster if we replace "state" with the actual state like (0, 0)
+        # I think for readability, it would make sense to do it like:
+        # {(0, 0): {"State": (0, 0), "Action": (0, 1), "Reward": 0.75}}
+        # as we know this small packet is assiocated with (0, 0)
+        # 
+
+
+        # because otherwise,. it would be a list of dictionaries? We can use a dictionary of dictionaries for easy updatinhg. NO! Because where would state be stored lol.
+        # We'd want to use a sorted list, sorted on the coordinates to display state.
+        # when picking from this table, make sure to pick a legal move!
+
+        # TODO delete this!!
+        self.rewardsTable = { (1, 1): {"State": (1, 1), "Action": (0, 1), "Reward": 0.75}, (0, 0): {"State": (0, 0), "Action": (0, 1), "Reward": 0.75}}
 
         # possible actions
         self.possibleActions = {"Up": (1, 0), "Down": (-1, 0), "Left": (0, -1), "Right": (0, 1)}
@@ -22,21 +38,29 @@ class agent:
         self.max_epochs = 1000
 
         self.explore = 0.15
+        
+        # the agents internal state of the reward
+        self.Agentreward = 0.00
+        self.penaltyMoving = -0.05
 
-    def qAlgorithm(oldValue, learningRate, reward, gamma, maximum):
+    def qAlgorithm(self, oldValue, reward, maximum):
         """
         oldValue is the old value of the q table value
         reward is how much reward the agent gets, calculated by Reward
         gamma is the discount rate
         maximum is the maximum possible reward from any action the agent can take from its current state
         """
-        return oldValue + (learningRate * reward + gamma * maximum - oldValue)
-    def run():
+        return oldValue + (self.learningRate * reward + self.gamma * maximum - oldValue)
+
+    def run(self):
         for epoch in range(1, self.max_epochs):
             # a list containing all the steps taken in this epoch
             runCompleted = False
             coords = (0, 0)
-            steps = []
+            # steps is all the steps it took in the maze
+            # as we always start at the self.start position
+            steps = [self.start]
+            self.Agentreward = 0.00
             while not runCompleted:
                 # if we are at the goal, our run is completed!!!
                 if coords == self.goal:
@@ -48,12 +72,28 @@ class agent:
                 if exploreOrNot:
                     # need to choose legal moves
                     # randomy choose an action to take
-                    action = random.choice(list(self.possibleActions.keys()))
+                    legalMoves = self.maze.getLegalMoves()
+                    # action, old coords for q table
+                    # just need the reward of the movement now!
+                    action = random.choice(legalMoves)
+                    oldCoords = self.maze.getAgentLocation()
+                    # performs the movement
+                    self.maze.agentMovement(action)
+                    # now we just need to calculate the reward
+                    steps.append(oldCoords)
+                    # now we need to perform the action
+                else:
+                    print("yeet no else here yet!")
+                
 
 
 
-
-    def reward(coordinates):
+    def reward(self, agentLocation, oldLocation, steps):
+        """
+        agentLocation = current location of the agent
+        oldLocation = previous location of the agent
+        Steps = all the steps the agent has taken
+        """
         """
         if i record path it takes to get to the end result
         then once it reaches the end, i can update the q learning table to reflect this
@@ -87,8 +127,79 @@ class agent:
         game ends when agent has -0.5 * 10 (size of maze) points
 
         Eventually, the best path to the maze will be the one which incurs the least penalty after having explored the entire maze.
+        
+        # updated rewards
+        * -0.25 negaticve reward for visting a square it has already been to
+        * all squares it visits when it reaches goal gets +1 reward
+        * -0.5 reward every time it moves, encourages it to get to end faster
+        * Agent keeps internal state of reward it has collected thus far
+        
+        Game ends when:
+        * Agent has -0.5 * 10 (size of maze) points (stops infinite loops / senseless wandering)
+
+        # the rewards table should include the previous state, and the action it took, with the reward gained from it.
+        Every time the agent wants to move, it should consult the reward table
         """
-        return sum(coordinates) / sum(self.goal))
+        # agent takes a penalty for moving
+        totalReward = self.penaltyMoving
+
+        # agent takes a penalty for vising a square it has already been to
+        if agentLocation in steps:
+            totalReward += -0.25
+        
+        if agentLocation == self.goal:
+            # update all rewards if we've reached the goal
+            # TODO can't do this until we have a rewards table!
+            totalReward += 1
+        # get old reward
+        try:
+            oldReward = self.rewardsTable[oldLocation]['Reward']
+        except KeyError as e:
+            # item does not exist in rewards table yet
+            oldReward = 0.00
+        maximumPossibleReward = self.maximumRewardFromLegalMoves()
+
+        newReward = self.qAlgorithm(oldReward, totalReward, maximumPossibleReward)
+
+        # now we need to calculate maximum mvoement rward from all possible legal moves in this new position
+    
+    def maximumRewardFromLegalMoves(self):
+        """
+        goes through all legal moves
+        calculates maximum possible reward
+        needed for Q learning algorithm
+        """
+        moves = self.maze.getLegalMoves()
+        # for every coord in the legal moves
+        coords = list(moves.values())
+        # set reward of first legal move to max reward
+        # prevents bugs
+        try:
+            maxReward = self.rewardsTable[coords[0]]['Reward']
+        except KeyError as e:
+            maxReward = 0.00
+        for i in list(moves.values()[1::]):
+            try:
+                newReward = self.rewardsTable[i]['Reward']
+            except KeyError as e:
+                newReward = 0.00
+            if maxReward < newReward:
+                maxReward = newReward
+        return maxReward
+
+
+        
+
+        return "No return yet"
+    def sortRewardsTable(self):
+        # sorts the rewards table on state
+        # so logically, coordinates (0, 0) comes first
+        newlist = sorted(x, key=lambda k: k['State'])
+
+    def searchRewardsTable(self, oldCoords, newCoords):
+        # Searches the rewards table, returns the item. 
+        # if we can search instantly,. this function is not needed.
+        return None
 
 
 
--- maze.py
+++ maze.py
@@ -21,29 +21,38 @@ class maze:
         ])
         self.row = 10
         self.col = 10
+        self.agentLocation = (0, 0)
+        self.moves = {"Up": (1, 0), "Down": (-1, 0), "Left": (0, -1), "Right": (0, 1)}
 
-    def getMaze():
+    def getMaze(self):
         return self.maze
-    def getSizeOfMaze():
+    def getSizeOfMaze(self):
         return len(self.maze)
     def vectorAddition(self, tup1, tup2):
         # since we want to add coords together element wise
         # (0, 0) + (1, 1) = (0, 0, 1, 1). this makes the result (1, 1).
         # https://stackoverflow.com/questions/497885/python-element-wise-tuple-operations-like-sum
         return tuple(map(operator.add, tup1, tup2))
-
-
-    def getLegalMoves(self, coords):
+    def setAgentLocation(self, location):
+        # keeps a record of where the agent is currently located
+        self.agentLocation = location
+    def getAgentLocation(self):
+        return self.agentLocation
+    def agentMovement(self, action):
+        # Moves the agent
+        # gets first coords from action, and adds that to agents current location
+        # agent can only make legal moves
+        self.setAgentLocation(self.vectorAddition(action[next(iter(action))], self.getAgentLocation()))
+    def getLegalMoves(self):
         # 0 is a wall
         # 1 is valid
-        moves = {"Up": (1, 0), "Down": (-1, 0), "Left": (0, -1), "Right": (0, 1)}
+        moves = self.moves
         legalMoves = {}
-        for move in moves:
+        for move in self.moves:
             try:
-                print(self.vectorAddition(coords, moves[move]))
-                newCoords = self.vectorAddition(coords, moves[move])
+                newCoords = self.vectorAddition(self.agentLocation, moves[move])
                 # checks to see if there is an illegal move
-                negativeCoords = True if any(y < 0 for y in x) else False
+                negativeCoords = True if any(y < 0 for y in newCoords) else False
                 outsideMap = True if any(y > len(self.maze[0]) - 1 for y in newCoords) else False
                 illegalMove = True if negativeCoords + outsideMap == True else False
                 if not illegalMove and newCoords != 0:
@@ -54,10 +63,14 @@ class maze:
                 print("Index error")
                 # Not a valid move as it goes out of bounds
                 continue
+        # returns dictionary of legal moves
+        # (0, 0) input
+        # {'Up': (1, 0), 'Right': (0, 1)} output
         return legalMoves
 
 m = maze()
-coords = (5, 5)
+coords = (0, 0)
 print(coords)
-print(m.getLegalMoves(coords))
-# dist = np.sqrt(((a[:, None] - b[:, :, None]) ** 2).sum(0))
+m.setAgentLocation((0, 0))
+print(m.getLegalMoves())
+# dist = np.sqrt(((a[:, None] - b[:, :, None]) ** 2).sum(0)) 8said animation and ri
